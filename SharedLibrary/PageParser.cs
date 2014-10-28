f53c824f1621fe943c381e8e81fef1fd1ae4cb8a
Rafael Adnet Pinho <rafaadnet@gmail.com>
2014-10-28 20:01:29 -0200

    First code commit
    Projects creation
    + first parser method
    + first code for worker

---
+++ SharedLibrary/PageParser.cs
@@ -0,0 +1,33 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary
+{
+    public class PageParser
+    {
+        /// <summary>
+        /// Parse web page HTML by the parameter tag and count results
+        /// </summary>
+        /// <param name="response">HTML of the page</param>
+        /// <returns>Count of tag occurrence it finds</returns>
+        public int CountTag(string html, string tag)
+        {
+            // Loading Html Document with Play Store content
+            HtmlDocument map = new HtmlDocument();
+            map.LoadHtml(html);
+
+            // Checking for nodes of tag
+            HtmlNodeCollection nodes = map.DocumentNode.SelectNodes("//"+tag);
+
+            // Checking consistency
+            if (nodes == null)
+                return 0;
+
+            return nodes.Count;
+        }
+    }
+}
---
+++ Worker/Worker.cs
@@ -0,0 +1,30 @@
+﻿using BDC.BDCCommons;
+using SharedLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebUtilsLib;
+
+namespace WebWorker
+{
+    class Worker
+    {
+        static void Main(string[] args)
+        {
+            // Configuring Log Object Threshold
+            LogWriter.Threshold = TLogEventLevel.Information;
+            LogWriter.Info("Worker Started");
+
+            //Parser
+            PageParser parser = new PageParser();
+
+            // Creating Instance of Web Requests Server
+            WebRequests server = new WebRequests();
+
+
+
+        }
+    }
+}
